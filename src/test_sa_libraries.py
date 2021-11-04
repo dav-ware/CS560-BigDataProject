a95457ed846121ef4a5a6ce2a27632879436522e
dav-ware <daw0023@mix.wvu.edu>
2021-11-04 12:33:17 -0400

    change test data name

--- src/test_sa_libraries.py
+++ src/test_sa_libraries.py
@@ -1,10 +1,8 @@
 import pandas as pd 
 import csv
+from nltk.corpus import twitter_samples, stopwords
 
-with open('./tweet_sentiments_0.csv') as tweet_sentiments:
-    df = pd.read_csv(tweet_sentiments)
-    df.columns = ['target', 'id', 'date', 'flag', 'user', 'text']
+with open('./tweet_sentiments_negative.csv') as tweet_sentiments:
+    df_neg = pd.read_csv(tweet_sentiments)
+    df_neg.columns = ['target', 'id', 'date', 'flag', 'user', 'text']
     
-
-print(df)
-
