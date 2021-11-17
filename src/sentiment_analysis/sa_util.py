f1b9f4dd7091c95e0d14bc5c2a84013d9a5859ba
dav-ware <daw0023@mix.wvu.edu>
2021-11-16 20:13:23 -0500

    sa with probability

--- src/sentiment_analysis/sa_util.py
+++ src/sentiment_analysis/sa_util.py
@@ -1,13 +1,43 @@
+import re, string
 from typing import List
 from nltk.tokenize import TweetTokenizer
-from nltk.corpus import twitter_samples, stopwords
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
 
 
-
-def tokenize(input: List[str]) -> List[List[str]]:
-    tokenized_input = []
+def tokenize(sentence: str) -> List[str]:
     tt = TweetTokenizer()
-    for tweet in input:
-        tokenized_input.append(tt.tokenize(tweet))
-    return tokenized_input
+    return tt.tokenize(sentence)
 
+def tag_tokens(token_list: List[str]) -> List[tuple]:
+    return pos_tag(token_list)
+
+def lemmatize_token(token: str, tag: str) -> str:
+    lemmatizer = WordNetLemmatizer()
+    pos=''
+    if tag.startswith('NN'):
+        pos = 'n'
+    elif tag.startswith('VB'):
+        pos = 'v'
+    elif tag.startswith('JJ'):
+        pos = 'a'
+    elif tag.startswith('RB'):
+        pos = 'r'
+    return lemmatizer.lemmatize(token, pos) if pos != '' else lemmatizer.lemmatize(token)
+
+def clean_tokens(tagged_token_list: List[str]) -> List[str]:
+    cleaned_tokens = []
+    sw = stopwords.words('english')
+    for token, tag in tagged_token_list:
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
+        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+        token = re.sub("(#.+)","", token)
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in sw:
+            token = lemmatize_token(token, tag)
+            cleaned_tokens.append(token)
+    return cleaned_tokens
+
+def prep_for_nbc(cleaned_token_list):
+    for token in cleaned_token_list:
+        yield dict([token, True] for token in token)
--- src/sentiment_analysis/test_sa_libraries.py
+++ src/sentiment_analysis/test_sa_libraries.py
@@ -1,22 +1,45 @@
 import pandas as pd 
-import csv
 import sa_util as util
 from textblob import TextBlob
+from nltk import NaiveBayesClassifier, classify
 from nltk.corpus import twitter_samples
+import time
+import random
 
 with open('./tweet_sentiments_negative.csv', encoding = "ISO-8859-1") as tweet_sentiments:
     df_neg = pd.read_csv(tweet_sentiments)
     df_neg.columns = ['target', 'id', 'date', 'flag', 'user', 'text']
-
 with open('./tweet_sentiments_positive.csv', encoding = "ISO-8859-1") as tweet_sentiments:
     df_pos = pd.read_csv(tweet_sentiments)
     df_pos.columns = ['target', 'id', 'date', 'flag', 'user', 'text']
-
-#positive_tweets = df_pos['text'].to_list()
-negative_tweets = df_neg['text'].to_list()
+t0 = time.time()
 positive_tweets = twitter_samples.strings('positive_tweets.json')
-res=[]
-for i in range(1000):
-    p = TextBlob(positive_tweets[i])
-    
+negative_tweets = twitter_samples.strings('negative_tweets.json')
 
+clean_pos_tweets = []
+clean_neg_tweets = []
+for i in range(100):
+    pos_tagged_token_list = util.tag_tokens(util.tokenize(positive_tweets[i]))
+    clean_pos_tweets.append(util.clean_tokens(pos_tagged_token_list))
+    neg_tagged_token_list = util.tag_tokens(util.tokenize(negative_tweets[i]))
+    clean_neg_tweets.append(util.clean_tokens(neg_tagged_token_list))
+pos = util.prep_for_nbc(clean_pos_tweets)
+neg = util.prep_for_nbc(clean_neg_tweets)
+
+positive_dataset = [(dict, "Positive") for dict in pos]
+negative_dataset = [(dict, "Negative") for dict in neg]
+
+dataset = positive_dataset + negative_dataset
+random.shuffle(dataset)
+
+train_data = dataset[:100]
+test_data = []
+for i in dataset[100:]:
+    test_data.append(i[0])
+
+classifier = NaiveBayesClassifier.train(train_data)
+t1 = time.time()
+
+res = classifier.prob_classify_many(test_data)
+for i in res:
+    print(i.prob('Positive'), i.prob('Negative'), i.max())
