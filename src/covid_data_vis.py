fd3df367c64d9f7f7e1581a30f784378e2b255c8
dav-ware <daw0023@mix.wvu.edu>
2021-12-08 01:55:00 -0500

    .

--- src/covid_data_vis.py
+++ src/covid_data_vis.py
@@ -1,11 +1,14 @@
 import pandas as pd 
 import geopandas
+import tkinter
+# import matplotlib
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-state_abbreviations = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY', 'NYC','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT',
+state_abbreviations = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NYC','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT',
 'VA','WA','WV','WI','WY']
 
-with open('datasets/covid19_data.csv') as file:
+with open('data/covid19_data.csv') as file:
     df = pd.read_csv(file)
 
 df = df[df['state'].isin(state_abbreviations)]
--- src/twitter_api.py
+++ src/twitter_api.py
@@ -1,10 +1,23 @@
 import tweepy
 import json
 from os import environ
+import csv
+import re
 
 auth = tweepy.AppAuthHandler(environ.get('TWITTER_API_KEY'), environ.get('TWITTER_API_SECRET'))
 api = tweepy.API(auth)
 
-resp = api.search_full_archive('dev', '"covid-19" place:"new york city"', fromDate=202003010000, toDate=202005010000, maxResults=10)
+ab = 'AL'
+place = 'place:alabama'
 
-print(json.dumps(resp[0]._json, indent=4, sort_keys=True))
+cdc_resp = tweepy.Cursor(api.search_full_archive, 'dev', f'cdc OR fauci OR "dr fauci" OR "doctor fauci" OR "cdc guidelines" OR "cdc mandate" {place} lang:en', fromDate=202003010000, toDate=202008010000).items(10000)
+lockdown_resp = tweepy.Cursor(api.search_full_archive, 'dev', 'covid place:alabama lang:en', fromDate=202003010000, toDate=202008010000).items()
+mask_resp = tweepy.Cursor(api.search_full_archive, 'dev', 'covid place:alabama lang:en', fromDate=202003010000, toDate=202008010000).items()
+vaccine_resp = tweepy.Cursor(api.search_full_archive, 'dev', 'covid place:alabama lang:en', fromDate=202003010000, toDate=202008010000).items()
+
+cdc_rows = []
+for tweet in cdc_resp:
+     cdc_rows.append([re.sub(r'\n|\t', ' ', tweet.text)])
+with open(f'data/twitter_raw/202003-202008/cdc/{ab}', 'w', encoding='utf-8') as file: 
+    csv_writer = csv.writer(file)
+    csv_writer.writerows(cdc_rows)
