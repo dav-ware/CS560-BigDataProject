ef7c6c32ac2148e9fc151735b91ce065ff266fe6
dav-ware <daw0023@mix.wvu.edu>
2021-12-09 06:43:05 -0500

    almost there

--- src/sentiment_analysis/twitter_sa.py
+++ src/sentiment_analysis/twitter_sa.py
@@ -1,18 +1,18 @@
 import pandas as pd 
 import sa_util as util
-from nltk import NaiveBayesClassifier, classify
-import time
+from nltk import NaiveBayesClassifier
+import csv
 import random
+import os
 
-
-with open('datasets/test_tweet_sentiments_negative.csv', encoding = "ISO-8859-1") as tweet_sentiments:
+with open('data/negative.csv', encoding = "ISO-8859-1") as tweet_sentiments:
     df_neg = pd.read_csv(tweet_sentiments)
     df_neg.columns = ['target', 'id', 'date', 'flag', 'user', 'text']
-with open('datasets/test_tweet_sentiments_positive.csv', encoding = "ISO-8859-1") as tweet_sentiments:
+with open('data/positive.csv', encoding = "ISO-8859-1") as tweet_sentiments:
     df_pos = pd.read_csv(tweet_sentiments)
     df_pos.columns = ['target', 'id', 'date', 'flag', 'user', 'text']
-raw_positive_tweets = df_pos['text'].head(50000).to_list()
-raw_negative_tweets = df_neg['text'].head(50000).to_list()
+raw_positive_tweets = df_pos['text'].head(5000).to_list()
+raw_negative_tweets = df_neg['text'].head(5000).to_list()
 
 positive_tokens = util.tokenize_tweets(raw_positive_tweets)
 negative_tokens = util.tokenize_tweets(raw_negative_tweets)
@@ -24,37 +24,51 @@ lemmatized_positive_tweets = util.lemmatize_tagged_tweets(tagged_positive_tweets
 lemmatized_negative_tweets = util.lemmatize_tagged_tweets(tagged_negative_tweets)
 nbc_positive_tweets = util.prep_for_nbc(lemmatized_positive_tweets)
 nbc_negative_tweets = util.prep_for_nbc(lemmatized_negative_tweets)
+
 ptrain = ((dict, "Positive") for dict in nbc_positive_tweets)
 ntrain = ((dict, "Negative") for dict in nbc_negative_tweets)
-
-t0 = time.time()
 positive_dataset = list(ptrain)
 negative_dataset = list(ntrain)
-t1 = time.time()
-dataset = positive_dataset + negative_dataset
-random.shuffle(dataset)
+training_dataset = positive_dataset + negative_dataset
+random.shuffle(training_dataset)
+nb_model = NaiveBayesClassifier.train(training_dataset)
 
-train_data = dataset[50000:]
-test_data = dataset[:50000]
+header = ['state', 'sentiment']
+rows = []
+for subdir, dirs, files in os.walk('data/twitter_raw/biden'):
+    for filename in files:
+        row = []
+        filepath = subdir + os.sep + filename
+        df_temp = pd.read_csv(filepath)
+        state = filepath[-6:-4]
+        raw_tweets = df_temp.iloc[:,0].to_list()
 
-nb_model = NaiveBayesClassifier.train(train_data)
-accuracy = classify.accuracy(nb_model, test_data)*100
+        tokens = util.tokenize_tweets(raw_tweets)
+        cleaned_tokens = util.clean_tokenized_tweets(tokens)
+        tagged_tweets = util.tag_tokenized_tweets(cleaned_tokens)
+        lemmatized_tweets = util.lemmatize_tagged_tweets(tagged_tweets)
+        nbc_tweets = util.prep_for_nbc(lemmatized_tweets)
+        sa = nb_model.prob_classify_many(nbc_tweets)
 
-detupled_test_data = [i[0] for i in test_data]
-correct_prediction = [i[1] for i in test_data]
-res = nb_model.prob_classify_many(detupled_test_data)
-wrong_count = 0
-skip_count = 0
+        p_count = 0
+        n_count = 0
+        for i in range(len(sa)):
+            if sa[i].prob(sa[i].max()) >= 0.9:
+                if sa[i].max() == 'Positive':
+                    p_count += 1
+                else:
+                    n_count += 1
+        if n_count == 0:
+            n_count += 1
+        sentiment = p_count/n_count
+        row.append(state)
+        row.append(sentiment)
+        rows.append(row)
+        print(state)
+        
+with open('data/twitter_sentiments/biden.csv', 'w', encoding='utf-8') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(header)
+    csv_writer.writerows(rows)
 
-for i in range(50000):
-    if res[i].max() != correct_prediction[i]:
-        if res[i].prob(res[i].max()) >= 0.9995:
-            wrong_count += 1
-        else:
-            skip_count += 1
-
-t2 = time.time()
-confident_accuracy = round((50000-skip_count-wrong_count)/(50000-skip_count)*100, 2)
-print(f'time to convert generator to list: {t1-t0}')
-print(f'time to do SA: {t2-t1}')
-print(f"accuracy using full dataset: {accuracy}%\nconfident accuracy: {confident_accuracy}% using {(50000-skip_count)/50000*100}% of dataset")
+print(rows)
